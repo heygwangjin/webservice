f1ef8f40487520acc78f13a501b691de1f6f9f85
heygwangjin <cer3@ajou.ac.kr>
2022-01-03 23:09:54 +0900

    main 클래스 작성 및 컨트롤러와 테스트 코드 작성

---
+++ src/main/java/com/webservice/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.webservice.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// 프로젝트의 메인 클래스이며, @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 프로젝트의 최상단에 위치해야 한다.
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 내장 WAS 실행 (서버에 톰캣 설치 필요 x)
+    }
+}

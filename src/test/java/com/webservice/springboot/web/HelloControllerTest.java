2c94df76541088bb67f21daa54e3121730535c79
heygwangjin <cer3@ajou.ac.kr>
2022-01-13 23:30:41 +0900

    버전 업데이트
    - Spring Boot 2.4.1
    - Gradle 6.7.1
    - Junit5

--- src/test/java/com/webservice/springboot/web/HelloControllerTest.java
+++ src/test/java/com/webservice/springboot/web/HelloControllerTest.java
@@ -1,18 +1,23 @@
 package com.webservice.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class) // SpringRunner는 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 한다.
+@ExtendWith(SpringExtension.class) // SpringRunner는 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 한다.
 @WebMvcTest(controllers = HelloController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 public class HelloControllerTest {
 
     @Autowired
